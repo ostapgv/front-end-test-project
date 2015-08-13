207398a5f41b324449bccd31fb86021a222f8b32
Grigory Ostapenko <gmostapgv@gmail.com>
2015-08-13 16:56:53 +0200

    fixed a bug with the showing first part of images even if we close the colorbox

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -5,7 +5,6 @@ $(function() {
         rel: "group1", 
         open: true,
         width: 250
-        //loop: false 
       }
     );
 });
@@ -14,10 +13,17 @@ $(document).bind("cbox_complete", function(){
   var $element = $.colorbox.element();
   
   if(!$element.hasClass("last")){
-      setTimeout($.colorbox.next, 2000);
+      $(this).data({timeout_id: setTimeout($.colorbox.next, 2000)});
   }else{
       setTimeout($.colorbox.close, 2000);
-      $(document).unbind("cbox_complete");
+      $(this).unbind("cbox_complete");
   }
-  
 });
+
+$(document).bind("cbox_closed", function(){
+  clearTimeout($(this).data().timeout_id);
+});
+
+//function s(){
+  
+//}
